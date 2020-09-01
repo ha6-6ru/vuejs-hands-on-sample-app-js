99181a5801e7c849d32b5eb68bed986a573509a7
ha6-6ru <20640973+ha6-6ru@users.noreply.github.com>
2020-09-02 01:56:39 +0900

    at the time of publication

---
+++ src/components/MyExample.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-text-field v-model="computedFooValue" v-bind="$attrs" />
+    <v-btn @click="click">click!</v-btn>
+  </div>
+</template>
+
+<script>
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  computed,
+} from '@vue/composition-api';
+import MixinExample from '@/mixins/mixin-example';
+
+export default defineComponent({
+  mixins: [MixinExample],
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const state = reactive({
+      computedFooValue: computed({
+        get: () => {
+          return props.value.foo;
+        },
+        set: value => {
+          context.emit('input', {
+            foo: value,
+            bar: props.value.bar,
+          });
+        },
+      }),
+    });
+    const click = () => {
+      context.emit('custom-event', 100);
+    };
+
+    return {
+      ...toRefs(state),
+      click,
+    };
+  },
+});
+</script>
